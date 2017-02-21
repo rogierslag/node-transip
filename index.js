2ab391491a439b9e1c7ffb4617178e69781e0881
Rogier Slag <rogier.slag@gmail.com>
2017-02-21 14:02:21 +0100

    0.0.1

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ var TransIP = require('./transip');
 
 var transipInstance = new TransIP();
 
-// transipInstance.vpsService.stopVps('magnetme-vps29').then(data => {
-//   console.log(data);
-//   transipInstance.vpsService.startVps('magnetme-vps29').then(data => {
-//     console.log(data);
-//     transipInstance.vpsService.getVpsBackupsByVps('magnetme-vps29').then(data => console.log(data));
-//   });
-// });
-
-// transipInstance.vpsService.getVpsBackupsByVps('magnetme-vps29').then(data => console.log(data));
+// transipInstance.vpsService.ssByVps('magnetme-vps29').then(data => console.log(data));
 transipInstance.haipService.getHaips().then(data => console.log(data));
+transipInstance.vpsService.getTrafficInformationForVps('magnetme-vps29').then(data => console.log(data));
+transipInstance.vpsService.getVpsBackupsByVps('magnetme-vps29').then(data => console.log(data));
+transipInstance.vpsService.startVps('magnetme-vps29').then(data => console.log(data));
